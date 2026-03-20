2e98373eb535f96cf36c629c9cc4daf3b3de9e79
ericgeiger1 <ericgeiger@yahoo.com>
2026-03-19 23:00:12 -0500

    Added go compiler

--- init.lua
+++ init.lua
@@ -32,7 +32,7 @@ require("lazy").setup({
     dependencies = { "williamboman/mason-lspconfig.nvim", "neovim/nvim-lspconfig" },
     config = function()
       require("mason").setup()
-      require("mason-lspconfig").setup({ ensure_installed = { "clangd", "ansiblels", "lua_ls" } })
+      require("mason-lspconfig").setup({ ensure_installed = { "clangd", "ansiblels", "lua_ls", "gopls" } })
     end
   },
 
